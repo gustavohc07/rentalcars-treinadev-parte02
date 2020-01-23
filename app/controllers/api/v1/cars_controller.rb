86a08f8c30f22aa2a7f9bd6f3315fb364720bdca
Gustavo e Alexandre <gustavo.hcarvalho@outlook.com>
2020-01-22 22:05:38 -0300

    WIP: create, update car
    Co-authored-by: Alexandre Moreira
    <39775226+alemlima@users.noreply.github.com>

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -12,4 +12,28 @@ class Api::V1::CarsController < Api::V1::ApiController
     @cars = Car.all
     render json: @cars
   end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.valid?
+      @car.save
+      render json: 'Created successfully', status: :created 
+    else
+      render json: 'Validation error', status: 412
+    end
+  end
+  
+  def update 
+    @car = Car.find_by(id: params[:id] )
+    @car.update(car_params)
+    render json: 'Updated successfully', status: :ok
+
+  end
+
+  private
+
+  def car_params
+    params.permit(:car_km, :license_plate, :color,
+                                :subsidiary_id, :car_model_id)
+  end
 end
--- spec/requests/car_management_spec.rb
+++ spec/requests/car_management_spec.rb
@@ -9,6 +9,8 @@ require 'rails_helper'
         json = JSON.parse(response.body, symbolize_names: true)
 
         expect(response).to have_http_status(:ok)
+        
+
         expect(json[:car_model_id]).to eq(car.car_model_id)    
         expect(json[:license_plate]).to eq(car.license_plate)    
         expect(json[:car_km]).to eq(car.car_km)    
@@ -34,20 +36,76 @@ require 'rails_helper'
 
         json = JSON.parse(response.body, symbolize_names: true)
 
-        pp json
         expect(response).to have_http_status(:ok)
-        expect(json[:car_model_id]).to eq(car.car_model_id)    
-        expect(json[:license_plate]).to eq(car.license_plate)    
-        expect(json[:car_km]).to eq(car.car_km)    
-        expect(json[:color]).to eq(car.color)    
-        expect(json[:subsidiary_id]).to eq(car.subsidiary_id)
+        expect(json[0][:car_model_id]).to eq(car.car_model_id)    
+        expect(json[0][:license_plate]).to eq(car.license_plate)    
+        expect(json[0][:car_km]).to eq(car.car_km)    
+        expect(json[0][:color]).to eq(car.color)    
+        expect(json[0][:subsidiary_id]).to eq(car.subsidiary_id)
 
-        expect(json[:car_model_id]).to eq(car2.car_model_id)    
-        expect(json[:license_plate]).to eq(car2.license_plate)    
-        expect(json[:car_km]).to eq(car2.car_km)    
-        expect(json[:color]).to eq(car2.color)    
-        expect(json[:subsidiary_id]).to eq(car2.subsidiary_id)
+        expect(json[1][:car_model_id]).to eq(car2.car_model_id)    
+        expect(json[1][:license_plate]).to eq(car2.license_plate)
+        expect(json[1][:car_km]).to eq(car2.car_km)
+        expect(json[1][:color]).to eq(car2.color)
+        expect(json[1][:subsidiary_id]).to eq(car2.subsidiary_id)
+
+        expect(json.size).to eq(2)
       end
+
+      context 'create' do
+        it 'create a car correctly' do
+          subsidiary = create(:subsidiary)
+          car_model = create(:car_model)
+
+          post api_v1_cars_path, params: 
+                                              {
+                                                car_km: 1000,
+                                                license_plate: 'ABC1234',
+                                                color: 'Azul',
+                                                subsidiary_id: subsidiary.id,
+                                                car_model_id: car_model.id,
+                                              }
+          expect(response).to have_http_status(:created)
+          expect(response.body).to include('Created successfully')
+
+          car = Car.last                                         
+          expect(car.car_km).to eq(1000)                                          
+          expect(car.license_plate).to eq('ABC1234')                                          
+          expect(car.color).to eq('Azul')
+        end
+
+        it 'should not create if missing parameter' do
+          post api_v1_cars_path, params: {}
+
+          expect(response).to have_http_status(412)
+          expect(response.body).to include('Validation error')
+
+        end
+      end
+
+      context 'update' do
+        it 'should update car successfully' do
+          car = create(:car, car_km: 1000, license_plate: 'ABC1234', color: 'Azul')
+
+          put api_v1_car_path(car.id), params: {
+                                              car_km: 20000,
+                                              license_plate: 'DEF5678',
+                                              color: 'Preto',
+                                             }
+
+         expect(response).to have_http_status(:ok)
+         expect(response.body).to include('Updated successfully')
+         car.reload
+         
+         expect(car.car_km).to eq(20000)
+         expect(car.license_plate).to eq('DEF5678')
+         expect(car.color).to eq('Preto')
+        end
+
+        #incluir verificação create ao invés de update
+      end
+      
+      
     end
     
   end
